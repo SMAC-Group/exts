1eede24dd255246701c43e58d58b8b3c80fc39fa
James Joseph Balamuta <balamut2@illinois.edu>
2016-11-11 10:25:42 -0600

    Adds a diag_resid check

---
+++ R/utilities.R
@@ -0,0 +1,8 @@
+#' Check is class
+#'
+#' Performs a check to see inheritance
+#' @param x A \code{diag_resid} object
+#' @return A \code{boolean} indicating \code{TRUE} or \code{FALSE}
+is.diag_resid = function(x){
+  inherits(x, "diag_resid")
+}
